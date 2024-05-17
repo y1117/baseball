71bd744fe3c5118418f2c1cb16fa9847e914961d
Yunsub <sim861117@gmail.com>
2024-05-17 13:56:57 +0900

    [feature] p30

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -7,3 +7,8 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
 
     EXPECT_THROW(game.guess(string("12")), length_error);
 }
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+    Baseball game;
+
+    EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
